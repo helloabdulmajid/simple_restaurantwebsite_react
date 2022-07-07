b8ade21f05a8de2d0f2807954253ed5557b5036f
Abdul Majid <helloabdulmajid@gmail.com>
2022-07-07 15:01:58 +0530

    navbar add but filter not working right now

--- src/components/basics/Restuarent.jsx
+++ src/components/basics/Restuarent.jsx
@@ -4,9 +4,28 @@ import { useState } from 'react'
 import MenuCard from './MenuCard'
 
 const Restuarent = () => {
-  const [menuData,setMenuData]=useState(Menu)
+  const [menuData,setMenuData]=useState(Menu);
+  const filterItem=(category)=>{
+    const updatedList= Menu.filter((curElem)=>{
+      return curElem.category===category;
+    });
+    setMenuData(updatedList)
+  };
+
   return (
     <>
+    <nav className='navbar' >
+      <div className='btn-group'>
+        <button className='btn-group__item' onChange={() =>filterItem("breakfast")}>BreakFast</button>
+        <button className='btn-group__item'>Lunch</button>
+        <button className='btn-group__item'>Evening</button>
+        <button className='btn-group__item'>Dinner</button>
+        <button className='btn-group__item'>All</button>
+
+
+
+      </div>
+    </nav>
     <MenuCard menuData={menuData}/>
     
     </>
